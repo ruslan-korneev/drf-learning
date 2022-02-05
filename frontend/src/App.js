e2fee664560fd5795472f14f0b87d181013dde3d
Ruslan <kornerus@fjfi.cvut.cz>
2022-02-05 21:21:36 +0300

    add Author component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import AuthorList from './components/Author';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      'authors': [
+        {
+          'first_name': 'Фёдор',
+          'last_name': 'Достоевский',
+          'birthday_year': 1821,
+        },
+        {
+            'first_name': 'Александр',
+            'last_name': 'Грин',
+            'birthday_year': 1880,
+        }
+      ]
+    };
+  };
+
+  render() {
+    return (
+      <div>
+        <AuthorList authors={this.state.authors}/>
+      </div>
+    );
+  }
+};
 
 export default App;
